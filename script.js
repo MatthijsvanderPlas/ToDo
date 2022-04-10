c5f870986c4f53d5754f0e84bce9c41e21d2b76c
Matthijs van der Plas <mvdplas84@ziggo.nl>
2022-04-10 11:41:16 +0200

    functionality

--- script.js
+++ script.js
@@ -1,61 +1,13 @@
-document.getElementById('addProject-el').addEventListener('click', addProject);
+document.getElementById("addTask-el").addEventListener("click", addTask);
 
-function addProject(e) {
-    e.preventDefault();
-    var todo = document.getElementById('addProject-el').value;
-    if (todo === "") {
-        return;
-    } else {
-        const todoItem = document.createElement('li')
-        todoItem.classList.add('todoItem')
-        todoItem.classList.toggle('checked')
-        todoItem.innerHTML = todo
-        document.querySelector('ul').appendChild(todoItem)
-        document.getElementById('addProject-el').value = ""
-    }
+function addTask(e) {
+  console.log(e);
+  e.preventDefault();
+  const input = document.getElementById("input-el").value;
+  const taskList = document.getElementById("tasks");
+  const newTask = document.createElement("li");
+  newTask.innerHTML = input;
+  newTask.classList.add("taskItem");
+  taskList.appendChild(newTask);
+
 }
-
-var myTaskList = document.getElementsByTagName('li');
-
-for (let i of myTaskList) {
-    var span = document.createElement('span');
-    var txt = document.createTextNode("X")
-    span.className = "close";
-    span.appendChild(txt);
-    myTaskList[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-for (let i of close){
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-function newElement(e) {
-    e.preventDefault();
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
