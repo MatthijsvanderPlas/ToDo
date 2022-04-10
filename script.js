31c3452da570b70a51f30c7b582f9b58aa894e0d
Matthijs van der Plas <mvdplas84@ziggo.nl>
2022-04-10 19:59:24 +0200

    Full functionality

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 document.getElementById("addTask-el").addEventListener("click", addTask);
 
 function addTask(e) {
-  console.log(e);
   e.preventDefault();
   const input = document.getElementById("input-el").value;
   const taskList = document.getElementById("tasks");
-  const newTask = document.createElement("li");
+  const newTask = document.createElement("input");
+  const container = document.createElement("div");
+  newTask.setAttribute("readonly", true);
+  newTask.setAttribute("id", "task-el")
+  const check = document.createElement("i");
+  const trash = document.createElement("i")
+  check.classList.add("fa", "fa-check", "fa-2x", "check");
+  trash.classList.add("fa", "fa-trash", "fa-2x", "trash");
+  check.setAttribute("id", "check-el");
+  check.setAttribute("id", "trash-el");
+  check.addEventListener("click", checkTask);
+  trash.addEventListener("click", deleteTask);
+  container.classList.add("itemBox");
+
+  newTask.value = input;
   newTask.innerHTML = input;
+  document.getElementById("input-el").value = "";
   newTask.classList.add("taskItem");
-  taskList.appendChild(newTask);
+  container.appendChild(newTask);
+  container.appendChild(check);
+  container.appendChild(trash);
+  taskList.appendChild(container);
+}
+
+function checkTask(e) {
+  e.preventDefault();
+  let task = e.path[0];
+  task = task.previousElementSibling;
+  task.classList.toggle("line-through");
+}
+
+function deleteTask(e) {
+  e.preventDefault();
+  let task = e.path[1];
+  task.parentNode.removeChild(task);
 
 }
